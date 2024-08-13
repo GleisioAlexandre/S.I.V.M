52caf35ad16751b533d563411d296fdf093a79d0
Gleisio <gleisio@gmail.com>
2024-08-13 18:41:06 -0300

    V1.0.4

---
+++ .vs/S.I.V.M/view/Frm_Produto.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace S.I.V.M.view
+{
+    partial class Frm_Produto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgProduto = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProduto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgProduto
+            // 
+            this.dtgProduto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgProduto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgProduto.Location = new System.Drawing.Point(0, 298);
+            this.dtgProduto.Name = "dtgProduto";
+            this.dtgProduto.Size = new System.Drawing.Size(1253, 375);
+            this.dtgProduto.TabIndex = 0;
+            // 
+            // Frm_Produto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 671);
+            this.Controls.Add(this.dtgProduto);
+            this.Name = "Frm_Produto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_Produto";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProduto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgProduto;
+    }
+}
